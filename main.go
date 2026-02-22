b85fe2ef5aff0313589be6098ebe28a8c9269a8b
sonic-ci-gen <sonic-ci.gen@cisco.com>
2026-02-22 13:06:03 -0800

    text box and button with window size and importance
    Signed-off-by: sonic-ci-gen <sonic-ci.gen@cisco.com>

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+type App struct {
+	output *widget.Label
+	button *widget.Button
+	entry *widget.Entry
+}
+
+func (a *App) MakeUI() {
+	a.output = widget.NewLabel("Hello, World!")
+	a.entry = widget.NewEntry()
+	a.button = widget.NewButton("Enter", func() {
+		a.output.SetText(a.entry.Text)
+	})
+	a.button.Importance = widget.HighImportance
+}
+
 func main() {
     a := app.New()
+	myApp := &App{}
+
 	window := a.NewWindow("Hello World")
-	label := widget.NewLabel("Hello, Fyne!")
-	window.SetContent( container.NewVBox(
-		label,
-		widget.NewButton("Click me", func(){
-			label.SetText("Button clicked")
-		}),
-	))
+	myApp.MakeUI()
+
+	window.SetContent(container.NewVBox(myApp.output, myApp.entry, myApp.button))
+	window.Resize(fyne.Size{Width: 500, Height: 500})
 	window.ShowAndRun()
 }
